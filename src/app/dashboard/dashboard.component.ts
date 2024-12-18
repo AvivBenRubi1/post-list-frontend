23b0a80b590d502f81307c385532afe7f945f258
Aviv Ben Rubi <avivbenrubi1@gmail.com>
2024-12-18 10:27:07 +0200

    fix posts style

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { PostComponent } from "../shared/components/post/post.component";
 import { Post } from '../models/post.model';
-
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import { CommonModule } from '@angular/common'
+import {IconFieldModule} from 'primeng/iconfield'
+import {InputIconModule}from 'primeng/inputicon';
+import {InputTextModule} from 'primeng/inputtext';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [PostComponent],
+  imports: [PostComponent, ButtonModule, CardModule, CommonModule, IconFieldModule, InputIconModule, InputTextModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
   testPost: Post = {
     author: "aviv",
-    content: "my first post",
-    title: "my title"
+    content: "my first post dfgdf g gdfgdfdg gdfgd dfgdf gdfgdfgd fg df gdfg dfgd fgd dfgdf gdg d gdg dzg dfg srfdg fhg dsf ghdf hdgfh dfgh dgf hdgf",
+    title: "my title",
+    likes: 8
   }
+  posts = [this.testPost, this.testPost]
+
 }
